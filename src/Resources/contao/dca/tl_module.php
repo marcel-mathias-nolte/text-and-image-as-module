5fe82c31fce2f363a829a7b40b3a152b814ae835
marcel-mathias-nolte <github@noltecomputer.de>
2020-10-26 10:07:35 +0100

    Fixed namespace in DCA

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -127,7 +127,7 @@ class ContaoTextAndImageAsModuleDcaHelper extends \Backend
 	 * Dynamically add flags to the "singleSRC" field
 	 *
 	 * @param mixed         $varValue
-	 * @param DataContainer $dc
+	 * @param \DataContainer $dc
 	 *
 	 * @return mixed
 	 */
@@ -152,7 +152,7 @@ class ContaoTextAndImageAsModuleDcaHelper extends \Backend
 	 * Pre-fill the "alt" and "caption" fields with the file meta data
 	 *
 	 * @param mixed         $varValue
-	 * @param DataContainer $dc
+	 * @param \DataContainer $dc
 	 *
 	 * @return mixed
 	 */
@@ -163,7 +163,7 @@ class ContaoTextAndImageAsModuleDcaHelper extends \Backend
 			return $varValue;
 		}
 
-		$objFile = FilesModel::findByUuid($varValue);
+		$objFile = \FilesModel::findByUuid($varValue);
 
 		if ($objFile !== null)
 		{
@@ -176,7 +176,7 @@ class ContaoTextAndImageAsModuleDcaHelper extends \Backend
 
 				if ($objPage->numRows)
 				{
-					$objModel = new PageModel();
+					$objModel = new \PageModel();
 					$objModel->setRow($objPage->row());
 					$objModel->loadDetails();
 
@@ -198,13 +198,13 @@ class ContaoTextAndImageAsModuleDcaHelper extends \Backend
 	/**
 	 * Return the link picker wizard
 	 *
-	 * @param DataContainer $dc
+	 * @param \DataContainer $dc
 	 *
 	 * @return string
 	 */
 	public function pagePicker(\DataContainer $dc)
 	{
-		return ' <a href="' . ((strpos($dc->value, '{{link_url::') !== false) ? 'contao/page.php' : 'contao/file.php') . '?do=' . \Input::get('do') . '&amp;table=' . $dc->table . '&amp;field=' . $dc->field . '&amp;value=' . str_replace(array('{{link_url::', '}}'), '', $dc->value) . '&amp;switch=1' . '" title="' . specialchars($GLOBALS['TL_LANG']['MSC']['pagepicker']) . '" onclick="Backend.getScrollOffset();Backend.openModalSelector({\'width\':768,\'title\':\'' . specialchars(str_replace("'", "\\'", $GLOBALS['TL_LANG']['MOD']['page'][0])) . '\',\'url\':this.href,\'id\':\'' . $dc->field . '\',\'tag\':\'ctrl_'. $dc->field . ((\Input::get('act') == 'editAll') ? '_' . $dc->id : '') . '\',\'self\':this});return false">' . Image::getHtml('pickpage.gif', $GLOBALS['TL_LANG']['MSC']['pagepicker'], 'style="vertical-align:top;cursor:pointer"') . '</a>';
+		return ' <a href="' . ((strpos($dc->value, '{{link_url::') !== false) ? 'contao/page.php' : 'contao/file.php') . '?do=' . \Input::get('do') . '&amp;table=' . $dc->table . '&amp;field=' . $dc->field . '&amp;value=' . str_replace(array('{{link_url::', '}}'), '', $dc->value) . '&amp;switch=1' . '" title="' . specialchars($GLOBALS['TL_LANG']['MSC']['pagepicker']) . '" onclick="Backend.getScrollOffset();Backend.openModalSelector({\'width\':768,\'title\':\'' . specialchars(str_replace("'", "\\'", $GLOBALS['TL_LANG']['MOD']['page'][0])) . '\',\'url\':this.href,\'id\':\'' . $dc->field . '\',\'tag\':\'ctrl_'. $dc->field . ((\Input::get('act') == 'editAll') ? '_' . $dc->id : '') . '\',\'self\':this});return false">' . \Image::getHtml('pickpage.gif', $GLOBALS['TL_LANG']['MSC']['pagepicker'], 'style="vertical-align:top;cursor:pointer"') . '</a>';
 	}
 
 }
